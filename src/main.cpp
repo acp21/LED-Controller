7dc64deb3b1405afc5211566cbfcc9c35785425b
acp21 <57569721+acp21@users.noreply.github.com>
2020-04-01 21:07:54 -0400

    Initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+// LED-Control
+// Written by Adam Pohl
+
+#include <Arduino.h>
+#include <FastLED.h>
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
