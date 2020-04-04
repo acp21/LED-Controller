437cd769d5481cd341878b0dc001d4ac8eac21ca
acp21h <acpohl21@gmail.com>
2020-04-04 01:44:01 -0400

    Moving dot

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,27 @@
 // LED-Control
 // Written by Adam Pohl
 
+#define FASTLED_ESP8266_RAW_PIN_ORDER
+
 #include <Arduino.h>
 #include <FastLED.h>
 
+
+#define NUM_LEDS 120
+#define DATA_PIN 14
+
+CRGB leds[NUM_LEDS];
+
+
 void setup() {
-  // put your setup code here, to run once:
+  FastLED.addLeds<WS2812B, 14, GRB>(leds, NUM_LEDS);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+ void loop() {
+      for(int i = 0; i < NUM_LEDS; i++){
+        leds[i] = CRGB::Blue;
+        FastLED.show();
+        leds[i] = CRGB::Black;
+        delay(25);
+      }
+    }
