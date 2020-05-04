139d3f3a16c8200c2981eac1d122ae226210a692
acp21h <acpohl21@gmail.com>
2020-05-04 19:52:59 -0400

    Added breathing animation

---
+++ src/animations/breathe.cpp
@@ -0,0 +1,47 @@
+#include "breathe.h"
+
+Breathe::Breathe(int startingLed_, int range_, int totalTime_, int cycleTime_, bool limited_, int startingBrightness_, int endingBrightness_, bool playNext_){
+    startingLed = startingLed_;
+    range = range_;
+    totalTime = totalTime_ * 1000;
+    cycleTime = cycleTime_ * 1000;
+    playNext = playNext_;
+    startingBrightness = startingBrightness_;
+    endingBrightness = endingBrightness_;
+    brightness = startingBrightness;
+    up = true;
+    down = false;
+    
+
+    calculateWait();
+
+}
+
+void Breathe::next(){
+    Animation::first();
+    if(millis() - prevStep >= wait && !completed){
+        ledArr(startingLed, range) = CHSV(CRGB::Red, 255, brightness);
+        if(up){
+            brightness++;
+            if(brightness >= endingBrightness){
+                up = false;
+                down = true;
+            }
+        }
+        else if(down){
+            brightness--;
+            if(brightness <= startingBrightness){
+                up = true;
+                down = false;
+            }
+        }
+        Serial.println(brightness);
+        FastLED.show();
+        prevStep = millis();
+        Animation::finish();
+    }
+}
+
+void Breathe::calculateWait(){
+    wait = cycleTime / ((endingBrightness - startingBrightness) * 2);
+}
---
+++ src/animations/breathe.h
@@ -0,0 +1,28 @@
+#ifndef BREATHE_H
+#define BREATHE_H
+
+#include "animation.h"
+
+class Breathe : public Animation{
+
+    public:
+    // Brightness value to start at
+    int startingBrightness;
+    // Brightness value to end at
+    int endingBrightness;
+    // Is breathe in custom range?
+    bool limited;
+    // Is brightness currently increasing?
+    bool up;
+    // Is birghtness currently decreasing?
+    bool down;
+
+    Breathe(int startingLed_ = 0, int range_ = NUM_LEDS, int totalTime_ = 5, int cycleTime_ = 5, bool limited_ = false, int startingBrightness_ = 0, int endingBrightness_ = 255, bool playNext = false);
+
+    void next();
+    void calculateWait();
+
+};
+
+
+#endif
