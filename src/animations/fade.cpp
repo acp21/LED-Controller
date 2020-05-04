63b46251119c3f91f87e5e5945ba0d69b682282a
acp21h <acpohl21@gmail.com>
2020-05-04 17:20:07 -0400

    Added variable fade colors

--- src/animations/fade.cpp
+++ src/animations/fade.cpp
@@ -2,13 +2,20 @@
 
 // CRGBArray<120> ledArr; // Refrence to extern CRGBArray
 
-Fade::Fade(int startingLed_, int range_, int totalTime_, int cycleTime_, int brightness_, bool playNext){
+Fade::Fade(int startingLed_, int range_, int totalTime_, int cycleTime_, int brightness_, bool rgb_, int startingColor_, int endingColor_, bool playNext){
     startingLed = startingLed_;
     range = range_;
     totalTime = totalTime_ * 1000;
     cycleTime = cycleTime_ * 1000;
     playNext = playNext;
     brightness = brightness_;
+    rgb = rgb_;
+    startingColor = startingColor_;
+    endingColor = endingColor_;
+    if(!rgb){
+        up = true;
+        down = false;
+    }
 
     calculateWait();
 };
@@ -17,13 +24,41 @@ void Fade::next(){
     Animation::first();
     if(millis() - prevStep >= wait && !completed){
         ledArr(startingLed, range) = CHSV(hue, 255, brightness);
+        if(rgb){
+            hue++;
+            if(hue > 255){
+                hue = 0;
+            }
+        }
+        else{
+            // If hue currently increasing
+            if(up){
+                hue++;
+                // If hue passes endingColor
+                if(hue > endingColor){
+                    // Switch direction
+                    up = false;
+                    down = true;
+                }
+            }
+            // Else if hue currently decreasing
+            else if(down){
+                hue--;
+                // If hue passes startingColor
+                if(hue < startingColor){
+                    // Switch direction
+                    up = true;
+                    down = false;
+                }
+            }
+        }
+        Serial.println(hue);
         FastLED.show();
-        hue++;
         prevStep = millis();
         Animation::finish();  
     }
 }
 
 void Fade::calculateWait(){
-    wait = cycleTime / 255;
+    wait = cycleTime / (endingColor - startingColor);
 }
--- src/animations/fade.h
+++ src/animations/fade.h
@@ -7,8 +7,18 @@
 class Fade : public Animation{
 
     public:
+    // If true, cycle contains all colors
+    bool rgb;
+    // Color fade starts on: MUST BE LOWER VAL THAN endingColor
+    int startingColor;
+    // Color fade ends on: MUST BE HIGHER VAL THAN startingColor
+    int endingColor;
+    // Is hue currently increasing?
+    bool up;
+    // Is hue currently decreasing?
+    bool down;
 
-    Fade(int startingLed_ = 0, int range_ = NUM_LEDS, int totalTime_ = 5, int cycleTime_ = 5, int brightness = 255, bool playNext_ = false);
+    Fade(int startingLed_ = 0, int range_ = NUM_LEDS, int totalTime_ = 5, int cycleTime_ = 5, int brightness = 255, bool rgb_ = true, int startingColor_ = 0, int endingColor_ = 255, bool playNext_ = false);
 
     void next();
     void calculateWait();
